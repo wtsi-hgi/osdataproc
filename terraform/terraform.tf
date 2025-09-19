09f2e9791415322974da9e7364ab713a1546f7c6
Eric Hidari <eh19@sanger.ac.uk>
2025-09-19 15:45:01 +0000

    Fix duplicate required providers configuration

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,19 +1,4 @@
 terraform {
-  required_version = ">= 0.12"
-  required_providers {
-    openstack = {
-      source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.32.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.5.0"
-    }
-    cloudinit = {
-      source  = "hashicorp/cloudinit"
-      version = "~> 2.3.0"
-    }
-  }
   backend "local" {
     path = "/tmp/terraform-state/terraform.tfstate"
   }
