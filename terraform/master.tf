e682b3255c7f32c384886d0f508056e43c680602
Eric Hidari <eh19@sanger.ac.uk>
2026-06-11 12:59:50 +0000

    Update osdataproc

--- terraform/master.tf
+++ terraform/master.tf
@@ -1,9 +1,10 @@
 resource "openstack_compute_instance_v2" "spark_master" {
-  name        = "${local.name_prefix}-master"
-  image_name  = var.image_name
-  flavor_name = var.flavor_name
-  key_pair    = openstack_compute_keypair_v2.spark_keypair.id
+  name         = "${local.name_prefix}-master"
+  image_name   = var.image_name
+  flavor_name  = var.flavor_name
+  key_pair     = openstack_compute_keypair_v2.spark_keypair.id
   config_drive = true
+  user_data    = data.cloudinit_config.user_data.rendered
 
   dynamic "network" {
     for_each = module.networking.master_ports
